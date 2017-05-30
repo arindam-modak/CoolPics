ccff4a7897bd4e0c2c181f17dffabe058672ca38
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-05-31 00:37:49 +0530

    Update FileUpload.php

--- FileUpload.php
+++ FileUpload.php
@@ -1,69 +1,16 @@
 <?php
-	if(isset($_FILES['UploadFileField'])) {
-
-             $UploadName = $_FILES['UploadFileField']['name'];
-             $UploadTmp = $_FILES['UploadFileField']['tmp_name'];
-             $UploadType = $_FILES['UploadFileField']['type'];
-             
-             $UploadName = preg_replace("#[^a-z0-9.]#i","", $UploadName);
-             
-             if(!$UploadTmp) {
-                    die("No File Selected, Please Upload Again");
-             }else{
-                  move_uploaded_file($UploadTmp, "Upload/$UploadName");
-                  $id = "";
-	if(isset($_REQUEST['txtNameUpload']))
-	{
-		$name = $_REQUEST['txtNameUpload'];
-	}
-	else
-	{
-		$name = "";
-	}
-	
-	
-        $con = mysqli_connect("localhost", "root", "");
-	
-	mysqli_select_db($con, "");
-		$sql = "Insert into tblpics(colId,colName,colUrl) values('$id', '$name', 'Upload/$UploadName')";
-		
-		if($name=="")
-		{
-			echo "Error! some of  the required fields are empty!!";
-		}
-		else
-		{
-			if(mysqli_query($con, $sql)===true)
-			{
-				if(mysqli_affected_rows($con)>0)
-				{
-					echo "Record Add successfully";
-				}
-				else
-				{
-					echo "Record could not be addeded!!";
-				}
-			}
-			else
-			{
-				echo "Error! Query could not be run!!";
-			}
-		}mysqli_close($con);
-	
-              }
-         }
-         
-
-
-
+    session_start();
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CoolPics</title>
+	<title>Upload Pics</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="icon" href="Penguins.jpg">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en" />
 	<link rel="stylesheet" type="text/css" href="style.css" />
@@ -103,9 +50,9 @@
 			</li>
 		</ul>
 
-		<form class="navbar-form navbar-left" role="search" method="get" action="#">
+		<form class="navbar-form navbar-left" role="search" method="get" action="search.php">
 			<div class="form-group">
-				<input type="text" class="form-control" name="q" value="">
+				<input type="text" class="form-control" name="q" placeholder="Images by Username">
 			</div>
 			<button type="submit" class="btn btn-default">Search</button>
 		
@@ -113,19 +60,34 @@
 
 		<ul class="nav navbar-nav navbar-right">
 			<li class="">
-				<a href="addpic.html">	
+                               <a href="https://github.com/arindam-modak" style="text-decoration:none;"><i class="fa fa-github" aria-hidden="true"></i> ADM</a>
+                        </li>
+			<li class="">
+				<a href="addpic.php">	
 					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;
 					Add Pic
 				</a>
 			</li>
-                         <li class="">
+                        <li class="">
 				<a href="FileUpload.php">	
 					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;
 					Upload Pic
 				</a>
 			</li>
+                        <li class="">
+				<a href="register.php">	
+					<span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>&nbsp;
+					Register
+				</a>
+			</li>
+                        <li class="">
+				<a href="login.php">	
+					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;
+					login
+				</a>
+			</li>
 			<li class="">
-				<a href="https://github.com/arindam-modak">	
+				<a href="logout.php">	
 					<span class="glyphicon glyphicon-off" aria-hidden="true"></span>&nbsp;
 					Logout
 				</a>
@@ -141,8 +103,8 @@
   <p>Upload Your own cool pics!!!</p>
   <form class="form" id="FileUploadForm" action="FileUpload.php" method="POST" enctype="multipart/form-data">
       <div class="form-group">
-          <label for="email">Name:</label>
-          <input type="text" name="txtNameUpload" class="form-control" placeholder="Enter your name">
+          <label for="email">Pic Name:</label>
+          <input type="text" name="txtNameUpload" class="form-control" placeholder="Enter Pic Name (optional)">
       </div>
      <div class="form-group">
           <input type="file" class="form-control" name="UploadFileField" id="UploadFileField">
@@ -151,9 +113,67 @@
     
 </form>
 </div>
+<?php
+     if (isset($_SESSION['username']))
+        {
+               
+        }
+        else { $_SESSION['username']=""; }
+    if($_SESSION['username']==""){
+         header("Location: https://iit2016036.000webhostapp.com/pics.php?"); /* Redirect browser */
+        exit();
+    }
+	if(isset($_FILES['UploadFileField'])) {
+            echo "<center><h2>";
+             $UploadName = $_FILES['UploadFileField']['name'];
+             $UploadTmp = $_FILES['UploadFileField']['tmp_name'];
+             $UploadType = $_FILES['UploadFileField']['type'];
+             
+             $UploadName = preg_replace("#[^a-z0-9.]#i","", $UploadName);
+             
+             if(!$UploadTmp) {
+                    die("No File Selected, Please Upload Again");
+             }else{
+                  move_uploaded_file($UploadTmp, "Upload/$UploadName");
+                  $id = "";
+	if(isset($_REQUEST['txtNameUpload']))
+	{
+		$name = $_REQUEST['txtNameUpload'];
+	}
+	else
+	{
+		$name = "";
+	}
+	
+	$uname = $_SESSION['username'];
+        $con = mysqli_connect("localhost", "root", "");
+	
+	mysqli_select_db($con, "");
+		$sql = "Insert into tblpics(colId,colName,colUrl) values('$id', '$uname', 'Upload/$UploadName')";
+
+			if(mysqli_query($con, $sql)===true)
+			{
+				if(mysqli_affected_rows($con)>0)
+				{
+					echo "Your Pic is Added successfully";
+				}
+				else
+				{
+					echo "Record could not be addeded!!";
+				}
+			}
+			else
+			{
+				echo "Error! Query could not be run!!";
+			}
+		 echo "</h2></center>";
+                 mysqli_close($con);
+	
+              }
+         }
+         
 
 
-
-
+?>
 </body>
 </html>
