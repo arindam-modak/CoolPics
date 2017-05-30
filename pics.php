f36f311a0aead5503a0e54a2ef9728d298c4c1cb
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-05-30 12:20:01 +0530

    Update pics.php

--- pics.php
+++ pics.php
@@ -48,6 +48,12 @@
 					Add Pic
 				</a>
 			</li>
+                         <li class="">
+				<a href="FileUpload.php">	
+					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;
+					Upload Pic
+				</a>
+			</li>
 			<li class="">
 				<a href="https://github.com/arindam-modak">	
 					<span class="glyphicon glyphicon-off" aria-hidden="true"></span>&nbsp;
